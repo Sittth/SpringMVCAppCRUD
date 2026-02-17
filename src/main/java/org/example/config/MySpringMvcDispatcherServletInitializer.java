c729278af7d79f3b956da8772aa22ce51b275e66
Eugene <jonscorpions1@gmail.com>
2026-02-17 22:17:41 +0300

    Added Update logic, edit.html, and filter in MySpringMvcDispatcherServletInitializer

--- src/main/java/org/example/config/MySpringMvcDispatcherServletInitializer.java
+++ src/main/java/org/example/config/MySpringMvcDispatcherServletInitializer.java
@@ -1,6 +1,9 @@
 package org.example.config;
 
+import jakarta.servlet.ServletContext;
+import jakarta.servlet.ServletException;
 import org.jspecify.annotations.Nullable;
+import org.springframework.web.filter.HiddenHttpMethodFilter;
 import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
 
 public class MySpringMvcDispatcherServletInitializer extends AbstractAnnotationConfigDispatcherServletInitializer {
@@ -18,5 +21,16 @@ public class MySpringMvcDispatcherServletInitializer extends AbstractAnnotationC
     @Override
     protected String[] getServletMappings() {
         return new String[] {"/"};
+    }
+
+    @Override
+    public void onStartup(ServletContext ServletContext) throws ServletException {
+        super.onStartup(ServletContext);
+        registerHiddenFieldFilter(ServletContext);
+    }
+
+    private void registerHiddenFieldFilter(ServletContext ServletContext) {
+        ServletContext.addFilter("hiddenHttpMethodFilter",
+                new HiddenHttpMethodFilter()).addMappingForUrlPatterns(null, false, "/*");
     }
 }
