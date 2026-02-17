5ea4454961f01307805ba2070d3bc0d2737c84fe
Eugene <jonscorpions1@gmail.com>
2026-02-18 01:37:15 +0300

    Added validation for name, age, email

--- src/main/java/org/example/models/Person.java
+++ src/main/java/org/example/models/Person.java
@@ -1,9 +1,22 @@
 package org.example.models;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.Size;
+
 public class Person {
     private int id;
+
+    @NotEmpty(message = "Name should not be empty")
+    @Size(min = 2, max = 20, message = "Name should be between 2 and 20 characters")
     private String name;
+
+    @Min(value = 0, message = "Age should be greater than 0")
     private int age;
+
+    @NotEmpty(message = "Email should not be empty")
+    @Email(message = "Email should be valid")
     private String email;
 
     public Person() {}
